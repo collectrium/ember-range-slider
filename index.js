2338cbbfea3b44b537f2a35645d73bb5aad0d381
LevonHaroutyunyan1 <levon.harut3@gmail.com>
2021-08-25 13:26:20 +0400

    import module with fastboot transformation option

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 /* jshint node: true */
 "use strict";
 
+const FASTBOOT_TRANSFORMATION_OPTION = {
+  using: [
+    {
+      transformation: 'fastbootShim',
+    },
+  ],
+};
+
 module.exports = {
   name: "@upsilon/ember-range-slider",
 
   included(app) {
-    if (typeof FastBoot !== 'undefined') {
-      app.import('node_modules/hammerjs/hammer.js');
-    }
+    this._super.included(app);
+
+    let hasFastboot = this.project.findAddonByName('ember-cli-fastboot');
+    let importOptions = hasFastboot ? FASTBOOT_TRANSFORMATION_OPTION : {};
+    app.import(
+      'node_modules/hammerjs/hammer.js',
+      importOptions
+    );
   },
 };
